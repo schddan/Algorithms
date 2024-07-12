7a3f0e780692b258d6b8419dbcd88062a1ec3a34
schddan <danielschmitzsc@gmail.com>
2024-07-12 20:27:44 -0300

    Grapho basic implementation

---
+++ Data Structures/Graphos/grapho.py
@@ -0,0 +1,13 @@
+#This is about a simple implementation of graphos, using the dictionaries
+
+grapho = {}
+grapho["first"] = ["alice", "bob", "claire"]
+grapho["bob"] = ["anuj", "peggy"]
+grapho["alice"] = ["peggy"]
+grapho["claire"] = ["thom", "jhonny"]
+grapho["anuj"] = [] 
+grapho["peggy"] = []
+grapho["thom"] = []
+grapho["johnny"] = []
+
+#And that's it, we only need to say which element link to each other
